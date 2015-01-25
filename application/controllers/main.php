3250b80341c93923ff34a4e16641bfa9cbccee5c
carloscarcamo <carloscarcamo.m@gmail.com>
2015-01-24 19:12:08 -0600

    v1

---
+++ application/controllers/main.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+  function __construct() {
+    parent::__construct();
+  }
+
+  public function index() {
+    $this->load->view('home');
+  }
+}
---
+++ application/views/home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CI simple login | CSHL</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+  </body>
+<html>
